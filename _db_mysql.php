e38fe7a8a6a3fcd125d2ace255cf93fa4e23c784
orkka <vincent.barbay@gmail.com>
2013-07-09 15:28:27 +0200

    Update _db_mysql.php
    correction not(!) in db_w

--- _db_mysql.php
+++ _db_mysql.php
@@ -74,7 +74,7 @@ function db_w($refs) {
 				$where[] = $proper_key.' LIKE '.$str_val;
 			}elseif(strstr($key, "!")){
 				$proper_key = str_replace('!','',$key);
-				$str_val = ($value===null)?'null':'"'.db_escape($proper_key, $link).'"';
+				$str_val = ($value===null)?'null':'"'.db_escape($value, $link).'"';
 				$where[] = $proper_key.' != '.$str_val;
 			}else{
 				$str_val = ($value===null)?'null':'"'.db_escape($value, $link).'"';
